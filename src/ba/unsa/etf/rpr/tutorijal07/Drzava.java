2553c5c11a487d506b321ef979fefcf1c3a835cd
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-02 15:02:35 +0100

    Klasa Drzava

---
+++ src/ba/unsa/etf/rpr/tutorijal07/Drzava.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.tutorijal07;
+
+public class Drzava {
+    String naziv;
+    private int brojStanovnika;
+    private double[] temperature = new double[1000];
+    private Grad glavniGrad;
+
+}
