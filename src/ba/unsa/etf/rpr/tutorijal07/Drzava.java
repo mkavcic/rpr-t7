11b895f2d520e821d01c86fc004098dc0bbb32b9
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-02 15:08:28 +0100

    Klasa Drzava - konstruktor, getteri i setteri

--- src/ba/unsa/etf/rpr/tutorijal07/Drzava.java
+++ src/ba/unsa/etf/rpr/tutorijal07/Drzava.java
@@ -1,9 +1,43 @@
 package ba.unsa.etf.rpr.tutorijal07;
 
 public class Drzava {
-    String naziv;
+    private String naziv;
     private int brojStanovnika;
     private double[] temperature = new double[1000];
     private Grad glavniGrad;
 
+    public Drzava() {
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public double[] getTemperature() {
+        return temperature;
+    }
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
 }
