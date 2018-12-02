36cbc8cd931c2e31684dc7a3985fa06d19ade181
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-02 15:08:58 +0100

    Klasa UN - konstruktor, getteri i setteri

--- src/ba/unsa/etf/rpr/tutorijal07/UN.java
+++ src/ba/unsa/etf/rpr/tutorijal07/UN.java
@@ -4,4 +4,15 @@ import java.util.ArrayList;
 
 public class UN {
     private ArrayList<Drzava> drzave;
+
+    public UN() {
+    }
+
+    public ArrayList<Drzava> getDrzave() {
+        return drzave;
+    }
+
+    public void setDrzave(ArrayList<Drzava> drzave) {
+        this.drzave = drzave;
+    }
 }
