9cad19e3542bc59644941b64e714ac33612de780
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-02 15:07:38 +0100

    Klasa Grad - konstruktor, getteri i setteri

--- src/ba/unsa/etf/rpr/tutorijal07/Grad.java
+++ src/ba/unsa/etf/rpr/tutorijal07/Grad.java
@@ -5,4 +5,39 @@ public class Grad {
     private int brojStanovnika;
     private double povrsina;
     private double[] temperature = new double [1000];
+
+    public Grad() {
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public double getPovrsina() {
+        return povrsina;
+    }
+
+    public double[] getTemperature() {
+        return temperature;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public void setPovrsina(double povrsina) {
+        this.povrsina = povrsina;
+    }
+
+    public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+    }
 }
